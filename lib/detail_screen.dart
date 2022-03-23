511db24cf6d2d5a66b647e7bc868d2b6fa7e2bfa
DheaMaharani <dheamaharaniku@gmail.com>
2022-03-23 08:04:11 +0700

    StateManagement

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,2 +1,102 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app/detail_screen.dart';
+import 'package:bab3/model/tourism_place.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key, required this.place}) : super(key: key);
+
+  final TourismPlace place;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Image.asset(place.imageAsset),
+          Container(
+            margin: const EdgeInsets.only(top: 16.0),
+            child: Text(
+              place.name,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                fontFamily: 'Lobster',
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 16.0),
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.calendar_today),
+                    Text(place.day),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.access_time),
+                    Text(place.jam),
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    Icon(Icons.attach_money),
+                    Text(place.harga),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              place.description,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 16.0,
+                fontFamily: 'Oxygen',
+              ),
+            ),
+          ),
+          Container(
+            height: 150,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget> [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                        place.image1
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                        place.image2
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.asset(
+                        place.image3
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/model/tourism_place.dart
@@ -0,0 +1,25 @@
+class TourismPlace{
+  String name;
+  String location;
+  String imageAsset;
+  String image1;
+  String image2;
+  String image3;
+  String description;
+  String day;
+  String jam;
+  String harga;
+
+  TourismPlace({
+    required this.name,
+    required this.location,
+    required this.imageAsset,
+    required this.image1,
+    required this.image2,
+    required this.image3,
+    required this.description,
+    required this.day,
+    required this.jam,
+    required this.harga
+  });
+}
